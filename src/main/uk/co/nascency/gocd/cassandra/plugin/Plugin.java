ee7b650638062bca5881925f3f2e6c669b710025
Justin Holmes <justin@nascency.co.uk>
2015-09-28 22:08:42 +0100

    Added gitignore and basic wiring

--- src/main/uk/co/nascency/gocd/cassandra/plugin/Plugin.java
+++ src/main/uk/co/nascency/gocd/cassandra/plugin/Plugin.java
@@ -6,6 +6,8 @@ import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
 import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+import main.uk.co.nascency.gocd.cassandra.plugin.database.CassandraDatabase;
+
 import static java.util.Arrays.asList;
 
 import java.util.List;
@@ -14,9 +16,10 @@ public class Plugin implements GoPlugin {
     public static final String EXTENSION_NAME = "cassandra";
     private static final List<String> goSupportedVersions = asList("1.0");
 
+
     @Override
     public void initializeGoApplicationAccessor(GoApplicationAccessor goApplicationAccessor) {
-        System.setProperty("go.database.provider", "com.thoughtworks.go.postgresql.PostgresqlDatabase");
+        System.setProperty("go.database.provider", "main.uk.co.nascency.gocd.cassandra.plugin.database.CassandraDatabase");
     }
 
     @Override
--- src/main/uk/co/nascency/gocd/cassandra/plugin/database/CassandraDatabase.java
+++ src/main/uk/co/nascency/gocd/cassandra/plugin/database/CassandraDatabase.java
@@ -1,5 +1,9 @@
 package main.uk.co.nascency.gocd.cassandra.plugin.database;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ProtocolOptions;
+import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
+import com.datastax.driver.core.policies.LatencyAwarePolicy;
 import com.thoughtworks.go.database.QueryExtensions;
 
 import javax.sql.DataSource;
@@ -7,6 +11,19 @@ import java.io.File;
 import java.sql.SQLException;
 
 public class CassandraDatabase implements com.thoughtworks.go.database.Database {
+
+    private static final Cluster cluster = cassandraConnectionFactory();
+
+    public static Cluster getCluster() {
+        return cluster;
+    }
+
+    private static Cluster cassandraConnectionFactory() {
+        return Cluster.builder()
+                .addContactPoints("localhost")
+                .withCompression(ProtocolOptions.Compression.LZ4).build();
+    }
+
     @Override
     public String dialectForHibernate() {
         return null;
